a05c1b237b602b2b802222851a013bbbd021bb11
Dmitry Volikov <volikovd17@gmail.com>
2023-12-07 05:45:55 +0300

    [Assembly stuff]

--- news-blog/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ news-blog/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -52,7 +52,7 @@ namespace news_blog {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/news-blog;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/news-blog;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
